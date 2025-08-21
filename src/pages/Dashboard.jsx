f5c737fc506c0ed8c2f5424dd95da0839c870e85
baqar-abbas <baqar.abbas@hotmail.com>
2025-08-22 03:53:22 +0500

    KPI Cards - Data fetch in API call from JSON server and renderred in cards

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import KpiCards from "../components/KpiCards";
 
 const Dashboard = () => {
   return (
     <div>
-      <h2 className="text-2xl font-bold">Welcome to Dashboard</h2>
+      <h1 className="text-2xl font-bold mb-6 text-gray-800">
+        Welcome to Dashboard
+      </h1>
+      <KpiCards />
+      {/* Later: charts, tables, logs etc */}
     </div>
   );
 };
